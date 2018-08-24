abd855f5912054e991a6b51e144a421226661b53
Ned Bellavance <nbellavance@anexinet.com>
2018-08-24 14:27:45 -0400

    Add BYOK stuff

--- m0/AIPSetup.ps1
+++ m0/AIPSetup.ps1
@@ -35,6 +35,28 @@ Install-Module AzureRM -Force
 Add-AzureRMAccount
 Get-AzureRMSubscription
 
+#Create a Resource Group
+$ResourceGroup = Read-Host -Prompt "Resource Group Name"
+$Location = Read-Host -Prompt "Location for Resource Group and Key Vault"
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $Location
+
+#Create a Key Vault
+$Vault = Read-Host -Prompt "Vault Name"
+New-AzureRmKeyVault -Name $Vault -ResourceGroupName $ResourceGroup -Location $Location -Sku Standard
+
+#Create a Key
+$key = Add-AzureKeyVaultKey -VaultName $Vault -Name "contoso-aip-key" -Destination Software -Size 2048
+
+#Configure Vault Access
+Set-AzureRmKeyVaultAccessPolicy -VaultName $vault -ResourceGroupName $ResourceGroup -ServicePrincipalName 00000012-0000-0000-c000-000000000000 -PermissionsToKeys decrypt,sign,get
+
+#Configure BYOK
+Use-AadrmKeyVaultKey -KeyVaultKeyUrl $key.Id
+
+#Get the AADRM Keys
+Get-AadrmKeys
+#Set the new key as active
+Set-AadrmKeyProperties -KeyIdentifier "[Key ID]" -Active:$true
 
 
 
